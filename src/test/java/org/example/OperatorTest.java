1a1d8a7303185f1cf525b970e60fbf290b1a3355
yoonsik <ys.shin@okestro.com>
2023-12-17 18:26:55 +0900

    예외 변경

--- src/test/java/org/example/OperatorTest.java
+++ src/test/java/org/example/OperatorTest.java
@@ -12,8 +12,8 @@ class OperatorTest {
         Operator plus = (x, y) -> x + y;
         Operator multiply = (x, y) -> x * y;
         Operator divide = (x, y) -> {
-            if(y != 0) return x / y;
-            else throw new RuntimeException("0으로 나눌수 없다");
+            if(y == 0) throw new ArithmeticException("0으로 나눌 수 없습니다.");
+            return x / y;
         };
 
         assertEquals(0, minus.operator(1, 1));
