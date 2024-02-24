f7283cf86e66a370d63faa5f4628dc2e86b8049f
yoonsik <ys.shin@okestro.com>
2024-02-24 17:55:05 +0900

    스레드 safe하지 않은 예제 코드

--- src/main/java/org/example/ch5/DataSharingBtwThreads.java
+++ src/main/java/org/example/ch5/DataSharingBtwThreads.java
@@ -1,7 +1,71 @@
 package org.example.ch5;
 
 public class DataSharingBtwThreads {
+    public static void main(String[] args) throws InterruptedException {
 
+        PointCounter pointCounter = new PointCounter();
+        IncrementingThread incrementingThread = new IncrementingThread(pointCounter);
+        DecrementingThread decrementingThread = new DecrementingThread(pointCounter);
+
+        incrementingThread.start();
+//        incrementingThread.join();
+
+        decrementingThread.start();
+//        decrementingThread.join();
+
+        System.out.println("현재 Point: " + pointCounter.getPoints());
+    }
+
+    private static class IncrementingThread extends Thread {
+        private PointCounter pointCounter;
+
+        public IncrementingThread(PointCounter pointCounter) {
+            this.pointCounter = pointCounter;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            for (int i = 0; i < 10000; i++) {
+                pointCounter.increment();
+            }
+            System.out.println("IncrementingThread:" + this.pointCounter.getPoints());
+        }
+    }
+
+    private static class DecrementingThread extends Thread {
+        private PointCounter pointCounter;
+
+        public DecrementingThread(PointCounter pointCounter) {
+            this.pointCounter = pointCounter;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            for (int i = 0; i < 10000; i++) {
+                this.pointCounter.decrement();
+            }
+            System.out.println("DecrementingThread:" + this.pointCounter.getPoints());
+        }
+    }
+
+
+    private static class PointCounter {
+        private int point = 0;
+
+        public void increment() {
+            point++;
+        }
+
+        public void decrement() {
+            point--;
+        }
+
+        public int getPoints() {
+            return point;
+        }
+    }
 
 
 }
