3c9e8aa7b6fa9f43728552bc2fea2ba9479e63cf
yoonsik <ys.shin@okestro.com>
2024-03-24 19:57:13 +0900

    Data Race 해결

--- src/main/java/org/example/ch6/DataRace.java
+++ src/main/java/org/example/ch6/DataRace.java
@@ -26,8 +26,8 @@ public class DataRace {
     }
 
     static class SharedClass{
-        private int x = 0;
-        private int y = 0;
+        private volatile int x = 0;
+        private volatile int y = 0;
 
         public void increment(){
             x++;
