f4b2e68df6928b8d5e1b542695022749169101e9
yoonsik <ys.shin@okestro.com>
2024-03-28 21:48:19 +0900

    설명 추가

--- src/main/java/org/example/ch6/DataRace.java
+++ src/main/java/org/example/ch6/DataRace.java
@@ -2,6 +2,12 @@ package org.example.ch6;
 
 /**
  * Data Race발생 코드
+ *
+ * 상황 설명
+ * x++; y++;이 독립적인 연산이므로 CPU의 비순차적 명령어 처리기능을 통해, increment()의
+ * 연산이 순차적으로 발생하지 않는다! 또한 volatile이 없다면(적절한 동기화 메커니즘이 없다면)
+ * y > x 같은 상황을 다른 스레드에서 관측할 수 있다. 이는 결과적으로 예측 불가능한 상황이 초래
+ * 할 수 있어 Data Race의 예제인 것이다
  */
 public class DataRace {
     public static void main(String[] args) {
